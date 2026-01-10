cad1c0186ed4a2954677561d2bb1ba25062133f4
Sachinthaka Gayashan <sachinthakagayashan44@gmail.com>
2026-01-10 20:11:12 +0530

    Update AssemblyInfo with new informational version and cache input

--- obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
+++ obj/Debug/net10.0/ContosoPizza.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d535ff409232d2a46629125fff3624ebcc9e7cbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContosoPizza")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
